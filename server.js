4ca33ad4509565b578d403aa9c3b0cea88155721
211-P\211-P <genadii13@gmail.com>
2022-10-22 13:52:26 +0500

    use static file

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const express = require("express")
 const app = require("express")();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
@@ -15,6 +16,8 @@ app.get("/", (req, res) => {
         res.sendFile(__dirname + "/sites/index.html");
     }
 });
+
+app.use(express.static('./sites'))
 
 io.on("connection", function (socket) {
 
